f23c3e17cee1f5aedebca2c8cc83078332f6b2f6
Vanessa Panosso <panossovanessa@gmail.com>
2016-06-09 01:37:26 -0300

    Tela Principal

--- Principal.php
+++ Principal.php
@@ -2,13 +2,18 @@
 
 	<head>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">  	
+		<title>Page title</title>  	
+		<link rel="stylesheet" href="style.css" type="text/css" media="screen">  
+		
 	</head>
 
 	<body>
-	
+	<div id="botao" >
 		<button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>
 		<button type="button" class="btn btn-default btn-lg btn-block">Block level button</button>
-	
+	</div>
 	</body>
 
 </html>
