467a8fb29c542be2b15b9d4912878bb3278fd94f
Vanessa Panosso <panossovanessa@gmail.com>
2016-06-09 01:18:54 -0300

    Tela Inicial

---
+++ Principal.php
@@ -0,0 +1,17 @@
+<html>
+
+	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	</head>
+
+	<body>
+	
+		<button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button>
+		<button type="button" class="btn btn-default btn-lg btn-block">Block level button</button>
+	
+	</body>
+
+</html>
+
+<?php
+?>
--- TelaLogin.php
+++ TelaLogin.php
@@ -18,15 +18,7 @@
       <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
     </div>
   </div>
-  <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <div class="checkbox">
-        <label>
-          <input type="checkbox"> Remember me
-        </label>
-      </div>
-    </div>
-  </div>
+  
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-default">Sign in</button>
