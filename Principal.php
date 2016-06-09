614e43327a97485835ccab56d5745e450e881c60
Vanessa Panosso <panossovanessa@gmail.com>
2016-06-09 01:38:25 -0300

    Teste

--- Principal.php
+++ Principal.php
@@ -4,7 +4,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 		
 		<meta http-equiv="content-type" content="text/html; charset=utf-8">  	
-		<title>Page title</title>  	
+		  	
 		<link rel="stylesheet" href="style.css" type="text/css" media="screen">  
 		
 	</head>
